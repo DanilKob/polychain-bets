b637b90b56e61b67172e65357851fe023c573eaa
Danylo Kobzar <danylo.kobzar@oboard.io>
2026-03-18 23:56:49 +0100

    disable default Spring basic auth configuration

--- src/main/kotlin/com/polychain/bets/Application.kt
+++ src/main/kotlin/com/polychain/bets/Application.kt
@@ -2,8 +2,12 @@ package com.polychain.bets
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 
-@SpringBootApplication
+@SpringBootApplication(
+	// disable Spring Boot's autoconfiguration of the UserDetailsService' for basic authentication
+	exclude = [UserDetailsServiceAutoConfiguration::class]
+)
 class Application
 
 fun main(args: Array<String>) {
